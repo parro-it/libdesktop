ac92dbc94f7347fd78a540128e4178e4ab334543
Andrea Parodi <andrea@parro.it>
2020-10-06 09:07:11 +0200

    tweaks

--- includes/napi_utils.h
+++ includes/napi_utils.h
@@ -253,60 +253,13 @@ static char enabled_debug_module[100];
 static size_t env_var_size = UNINITIALIZED;
 #endif
 
+napi_value make_bool(napi_env env, bool value);
+napi_value make_double(napi_env env, double value);
+napi_value make_int32(napi_env env, int value);
+napi_value make_int64(napi_env env, long value);
+napi_value make_uint32(napi_env env, unsigned int value);
+napi_value make_utf8_string(napi_env env, const char *char_ptr);
 
-static inline napi_value make_bool(napi_env env, bool value) {
-	napi_value value_js;
-
-	napi_status status = napi_get_boolean(env, value, &value_js);
-	CHECK_STATUS_THROW(status, napi_get_boolean);
-
-	return value_js;
-}
-
-static inline napi_value make_double(napi_env env, double value) {
-	napi_value value_js;
-
-	napi_status status = napi_create_double(env, value, &value_js);
-	CHECK_STATUS_THROW(status, napi_create_double);
-
-	return value_js;
-}
-
-static inline napi_value make_int32(napi_env env, int value) {
-	napi_value value_js;
-
-	napi_status status = napi_create_int32(env, value, &value_js);
-	CHECK_STATUS_THROW(status, napi_create_int32);
-
-	return value_js;
-}
-
-static inline napi_value make_int64(napi_env env, long value) {
-	napi_value value_js;
-
-	napi_status status = napi_create_int64(env, value, &value_js);
-	CHECK_STATUS_THROW(status, napi_create_int64);
-
-	return value_js;
-}
-
-static inline napi_value make_uint32(napi_env env, unsigned int value) {
-	napi_value value_js;
-
-	napi_status status = napi_create_uint32(env, value, &value_js);
-	CHECK_STATUS_THROW(status, napi_create_uint32);
-
-	return value_js;
-}
-
-static inline napi_value make_utf8_string(napi_env env, const char *char_ptr) {
-	napi_value result;
-
-	napi_status status = napi_create_string_utf8(env, char_ptr, NAPI_AUTO_LENGTH, &result);
-	CHECK_STATUS_THROW(status, napi_create_string_utf8);
-
-	return result;
-}
 #if UI_NODE_DEBUG
 
 static inline bool debug_enabled_for_module(const char *module) {
---
+++ src/platforms/common/napiutils.c
@@ -0,0 +1,55 @@
+#include "napi_utils.h"
+
+napi_value make_bool(napi_env env, bool value) {
+	napi_value value_js;
+
+	napi_status status = napi_get_boolean(env, value, &value_js);
+	CHECK_STATUS_THROW(status, napi_get_boolean);
+
+	return value_js;
+}
+
+napi_value make_double(napi_env env, double value) {
+	napi_value value_js;
+
+	napi_status status = napi_create_double(env, value, &value_js);
+	CHECK_STATUS_THROW(status, napi_create_double);
+
+	return value_js;
+}
+
+napi_value make_int32(napi_env env, int value) {
+	napi_value value_js;
+
+	napi_status status = napi_create_int32(env, value, &value_js);
+	CHECK_STATUS_THROW(status, napi_create_int32);
+
+	return value_js;
+}
+
+napi_value make_int64(napi_env env, long value) {
+	napi_value value_js;
+
+	napi_status status = napi_create_int64(env, value, &value_js);
+	CHECK_STATUS_THROW(status, napi_create_int64);
+
+	return value_js;
+}
+
+napi_value make_uint32(napi_env env, unsigned int value) {
+	napi_value value_js;
+
+	napi_status status = napi_create_uint32(env, value, &value_js);
+	CHECK_STATUS_THROW(status, napi_create_uint32);
+
+	return value_js;
+}
+
+napi_value make_utf8_string(napi_env env, const char *char_ptr) {
+	napi_value result;
+
+	napi_status status = napi_create_string_utf8(env, char_ptr, NAPI_AUTO_LENGTH, &result);
+	CHECK_STATUS_THROW(status, napi_create_string_utf8);
+
+	return result;
+}
