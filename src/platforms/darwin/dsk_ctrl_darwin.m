e79ba41e2131e706c1114ac90069504c89fdc5e6
Andrea Parodi <andrea@parro.it>
2020-11-15 16:35:39 +0100

    fixes

--- src/platforms/darwin/dsk_ctrl_darwin.m
+++ src/platforms/darwin/dsk_ctrl_darwin.m
@@ -12,7 +12,11 @@
 
 void dsk_initui_for_test() {}
 
-void *dsk_new_test_widget() { return [[DskTestCtrl alloc] init]; }
+void *dsk_new_test_widget() {
+	DskTestCtrl *ctrl = [[DskTestCtrl alloc] init];
+	ctrl.stringValue = "Hello world";
+	return ctrl;
+}
 
 napi_status dsk_CtrlI_link_UIHandle(UIHandle UI_ctrl, DskCtrlI *ctrl) {
 	DskTestCtrl *ns_ctrl = UI_ctrl;
